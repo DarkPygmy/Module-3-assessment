adbba9d7a0f61eb06a3401fb99d3e20fc1af0876
David Louis <davloufl@gmail.com>
2024-02-09 14:37:43 -0600

    Am working on the rest of it today, anxiety attack caused issues, along with sleep issues.

---
+++ about.js
@@ -0,0 +1,21 @@
+console.log("hello world");
+
+
+
+function handleSubmit(evt) {
+	evt.preventDefault();
+	
+	alert("Form submitted successfully");
+}
+
+function mouseOverButton(evt) {
+	evt.preventDefault();
+	
+	alert("Do the best you can!");
+}
+
+let form = document.querySelector('#contact');
+let button = document.querySelector('#button')
+
+button.addEventListener('mouseover', mouseOverButton)
+form.addEventListener('submit', handleSubmit);
